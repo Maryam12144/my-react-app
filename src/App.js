b6ff3dc5e3b5b61d77a933842542daf90d54d6d9
Maryam Tufail <maryamtufail190@gmail.com>
2025-02-20 11:55:35 +0500

    Initial commit - React project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, GitHub Actions! 🚀</h1>
+      <p>This is a simple React project to learn CI/CD.</p>
+    </div>
+  );
+}
+
+export default App;
